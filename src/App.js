986f37d481099b13fbb3be58e14be1953fbe4169
jersoncarin-dev <jersoncarin@tmdcitsolutions.com>
2021-11-20 00:28:51 +0800

    Make heading clickable links

--- src/App.js
+++ src/App.js
@@ -107,14 +107,18 @@ const Main = () => {
         setHasAreaText(false)
     }, [text, image.hasImage, image.name])
 
+    const onAuthorClick = () => {
+        window.location.href = 'https://jersoncarin.dev'
+    }
+
     return (
         <div className="w-full bg-gray-800 min-h-screen">
             <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
                 <div className="text-center pb-12">
-                    <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
-                        CSS Portrait Image Generator
+                    <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white" style={{cursor: 'pointer'}} onClick={() => navigate('/')}>
+                        CSS Portrait Image Generator 🔥
                     </h1>
-                    <h2 className="text-base font-bold text-indigo-600 mt-2">
+                    <h2 className="text-base font-bold text-indigo-600 mt-2" onClick={onAuthorClick} style={{cursor: 'pointer'}}>
                         Made with ❤️ by Jerson Carin
                     </h2>
                 </div>
